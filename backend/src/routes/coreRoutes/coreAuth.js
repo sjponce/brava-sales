f66400b71804e2020b954244ecc382daa516ca6a
ebert-alem <ebertalem@gmail.com>
2024-05-28 22:16:29 -0300

    fix

--- backend/src/routes/coreRoutes/coreAuth.js
+++ backend/src/routes/coreRoutes/coreAuth.js
@@ -3,18 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 const { catchErrors } = require('@/handlers/errorHandlers');
-const adminAuth = require('@/controllers/coreControllers/adminAuth');
 const userAuth = require('@/controllers/coreControllers/userAuth');
 
-
-//router.route('/login').post(catchErrors(adminAuth.login));
-
-
-
-// router.route('/forgetpassword').post(catchErrors(adminAuth.forgetPassword));
-// router.route('/resetpassword').post(catchErrors(adminAuth.resetPassword));
-
-// router.route('/logout').post(userAuth.isValidAuthToken, catchErrors(adminAuth.logout));
 
 router.route('/login').post(catchErrors(userAuth.login));
 
