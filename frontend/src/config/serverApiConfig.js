6690069a4255424e65197e8b2669b36fb6e344fc
sjponce <santiago.j.ponce@gmail.com>
2024-05-29 14:45:22 -0300

    PFG82-96: Fixed test on frontend

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = `${import.meta.env.VITE_BACKEND_SERVER}api/` ?? 'http://localhost:8089/api/';
-export const BASE_URL = `${import.meta.env.VITE_BACKEND_SERVER}` ?? 'http://localhost:8070';
+export const API_BASE_URL = `${process.env.VITE_BACKEND_SERVER}api/` ?? 'http://localhost:8089/api/';
+export const BASE_URL = `${process.env.VITE_BACKEND_SERVER}` ?? 'http://localhost:8070';
 
 export const WEBSITE_URL = 'http://localhost:3000/';
 export const DOWNLOAD_BASE_URL = 'http://localhost:8080/download/';
