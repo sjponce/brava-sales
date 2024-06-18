dd1242701e18388b8ae59d385df6f70925e87c87
sjponce <santiago.j.ponce@gmail.com>
2024-06-18 20:25:35 -0300

    PFG82-65: Fixed bug where the modal can't be opened

--- frontend/src/pages/private/CommingSoon.jsx
+++ frontend/src/pages/private/CommingSoon.jsx
@@ -1,12 +1,11 @@
 import { Box, Typography } from '@mui/material';
 
-const CommingSoon = () => {
-  console.log('CommingSoon');
-  return (
-    <Box display="flex" justifyContent="center" height="100%" alignItems="center">
-      <Typography variant="h4" color="primary">Proximamente...</Typography>
-    </Box>
-  );
-};
+const CommingSoon = () => (
+  <Box display="flex" justifyContent="center" height="100%" alignItems="center">
+    <Typography variant="h4" color="primary">
+      Proximamente...
+    </Typography>
+  </Box>
+);
 
 export default CommingSoon;
