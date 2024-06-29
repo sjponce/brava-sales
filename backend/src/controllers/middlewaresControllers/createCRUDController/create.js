9666ae78ec85267252a7846835c6801c9af68806
sjponce <santiago.j.ponce@gmail.com>
2024-06-29 19:16:37 -0300

    PFG82-123: Added exceptions to crud controller

--- backend/src/controllers/middlewaresControllers/createCRUDController/create.js
+++ backend/src/controllers/middlewaresControllers/createCRUDController/create.js
@@ -1,6 +1,19 @@
+const modelExceptions = require('./modelExceptions');
+
 const create = async (Model, req, res) => {
   // Creating a new document in the collection
   req.body.removed = false;
+
+  // Checking if there are any exceptions for creation
+  const createExceptionFunctions = Object.values(modelExceptions[Model.modelName]?.create || {});
+
+  for (const exceptionFunction of createExceptionFunctions) {
+    const exceptionResult = await exceptionFunction(Model, req);
+    if (exceptionResult) {
+      return res.status(400).json(exceptionResult);
+    }
+  }
+
   const result = await new Model({
     ...req.body,
   }).save();
--- backend/src/controllers/middlewaresControllers/createCRUDController/update.js
+++ backend/src/controllers/middlewaresControllers/createCRUDController/update.js
@@ -1,4 +1,16 @@
+const modelExceptions = require("./modelExceptions");
+
 const update = async (Model, req, res) => {
+  // Checking if there are any exceptions for creation
+  const createExceptionFunctions = Object.values(modelExceptions[Model.modelName]?.create || {});
+
+  for (const exceptionFunction of createExceptionFunctions) {
+    const exceptionResult = await exceptionFunction(Model, req);
+    if (exceptionResult) {
+      return res.status(400).json(exceptionResult);
+    }
+  }
+
   // Find document by id and updates with the required fields
   req.body.removed = false;
   const result = await Model.findOneAndUpdate({ _id: req.params.id, removed: false }, req.body, {
