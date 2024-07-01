9bd931c81cd378318132461e9020fece7681f927
sjponce <santiago.j.ponce@gmail.com>
2024-07-01 12:17:31 -0300

    PFG82-134: Fix build

--- frontend/src/router/AuthRouter.jsx
+++ frontend/src/router/AuthRouter.jsx
@@ -4,7 +4,6 @@ import Login from '@/pages/Login';
 import NotFound from '@/pages/NotFound';
 
 import ForgetPassword from '@/pages/ForgetPassword';
-import ResetPassword from '@/pages/ResetPassword';
 
 const AuthRouter = () => (
   <Routes>
@@ -12,7 +11,6 @@ const AuthRouter = () => (
     <Route element={<Login />} path="/login" />
     <Route element={<Navigate to="/login" replace />} path="/logout" />
     <Route element={<ForgetPassword />} path="/forgetpassword" />
-    <Route element={<ResetPassword />} path="/resetpassword/:userId/:resetToken" />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
