3b1ac32e023cab03bbc5696ab49b94b58c185c22
ebert-alem <ebertalem@gmail.com>
2024-07-05 20:22:08 -0300

    edit-product

--- backend/src/controllers/appControllers/stock/index.js
+++ backend/src/controllers/appControllers/stock/index.js
@@ -1,10 +1,18 @@
 const listAll = require('./listAll');
 const { callWithAuth } = require('./callWithAuth');
+const { update } = require('./update');
+const { remove } = require('./remove');
 
 const createStockMiddleware = () => {
   const stockMethods = {
     listAll: async (req, res, ...args) => {
       await callWithAuth(req, res, listAll, ...args);
+    },
+    update: async (req, res, ...args) => {
+      await callWithAuth(req, res, update, ...args);
+    },
+    remove: async (req, res, ...args) => {
+      await callWithAuth(req, res, remove, ...args);
     },
   };
   return stockMethods;
---
+++ backend/src/controllers/appControllers/stock/update.js
@@ -0,0 +1,31 @@
+const update = async (req, res, axiosInstance) => {
+  try {
+    const apiUrl = `${process.env.STOCK_API}/product`;
+
+    const response = await axiosInstance.put(apiUrl, req.body);
+
+    return res.status(200).json({
+      success: true,
+      result: response.data,
+      message: 'Se actualizo el producto',
+    });
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      return res.status(404).json({
+        success: false,
+        result: null,
+        message: 'No se encontro el producto',
+      });
+    }
+
+    console.error('Error updating product from stock API:', error);
+    return res.status(500).json({
+      success: false,
+      result: null,
+      message: 'Ocurrio un error contactando a Stock',
+      error: error.message,
+    });
+  }
+};
+
+module.exports = update;
