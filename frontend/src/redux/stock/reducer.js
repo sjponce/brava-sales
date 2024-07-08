a71be2267888086d9171be270ace9e3ef398dbf0
ebert-alem <ebertalem@gmail.com>
2024-07-08 18:49:58 -0300

    PFG82-138: fix reducers code

--- frontend/src/redux/stock/reducer.js
+++ frontend/src/redux/stock/reducer.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import * as actionTypes from './types';
 
 const INITIAL_KEY_STATE = {
@@ -30,6 +29,7 @@ const INITIAL_STATE = {
   search: { ...INITIAL_KEY_STATE, result: [] },
 };
 
+// eslint-disable-next-line default-param-last
 const crudReducer = (state = INITIAL_STATE, action) => {
   const { payload, keyState } = action;
   switch (action.type) {
